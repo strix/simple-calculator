c022681eecb60867b94e04972e6a8141b4d543ac
Brance Boren <branceboren@gmail.com>
2018-04-05 22:42:38 -0600

    Added event listener for clicks and handler placeholders

--- calculator.js
+++ calculator.js
@@ -1,1 +1,29 @@
-console.log('I am sourced correctly!')
+window.addEventListener('click', e => {
+  switch (e.target.className) {
+    case 'number':
+      console.log('handle number')
+      console.log(Number(e.target.innerHTML) ? Number(e.target.innerHTML) : e.target.innerHTML)
+      break
+    case 'operation':
+      console.log('handle operation')
+      console.log(e.target.value)
+      break
+    case 'equal':
+      console.log('handle equal')
+      console.log(e.target.innerHTML)
+      break
+    case 'AC':
+      console.log('handle AC')
+      break
+    case 'CE':
+      console.log('handle CE')
+      break
+    default:
+      console.log('not recognized')
+      break
+  }
+})
+
+const calculator = () => {
+
+}
