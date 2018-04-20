68b1df4bef2c44d3053a0a1574fdd40905457bd1
Brance Boren <branceboren@gmail.com>
2018-04-19 22:15:49 -0600

    Added basic functionality to calculator "brain"

--- calculator.js
+++ calculator.js
@@ -1,12 +1,51 @@
+function Calculator () {
+  this.total = 0
+  this.operand = ''
+  this.handleOperand = (operand) => {
+    if (this.operand) {
+      // call function based on operand
+      this.setText(total)
+    }
+    this.operand = operand
+  }
+  this.textBox = document.getElementById('textBox')
+  this.add = (num) => {
+    this.total += num
+  }
+  this.subtract = (num) => {
+    this.total -= num
+  }
+  this.multiply = (num) => {
+    this.total *= num
+  }
+  this.divide = (num) => {
+    this.total /= num
+  }
+  this.setText = (text) => {
+    this.textBox.value = String(text)
+  }
+  this.clear = () => {
+    this.total = 0
+    this.setText('')
+  }
+  this.equals = () => {
+    this.setText(total)
+  }
+}
+
+const calculator = new Calculator()
+
 window.addEventListener('click', e => {
   switch (e.target.className) {
     case 'number':
       console.log('handle number')
-      console.log(Number(e.target.innerHTML) ? Number(e.target.innerHTML) : e.target.innerHTML)
+      const num = Number(e.target.innerHTML) ? Number(e.target.innerHTML) : e.target.innerHTML
+      calculator.setText(num)
       break
     case 'operation':
       console.log('handle operation')
       console.log(e.target.value)
+      calculator.handleOperand(e.target.value)
       break
     case 'equal':
       console.log('handle equal')
@@ -24,6 +63,3 @@ window.addEventListener('click', e => {
   }
 })
 
-const calculator = () => {
-
-}
